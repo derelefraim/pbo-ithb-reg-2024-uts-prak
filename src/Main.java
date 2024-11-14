7cae397b5245da3ccaa4e019534dac321934541a
derelefraim <145255845+derelefraim@users.noreply.github.com>
2024-11-14 16:08:33 +0700

    done

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,6 @@
 import View.*;
-
+// Asumsi Method yang ada di Diagram berada di Controller
+// Kata ko nidel gitu
 public class Main {
     public static void main(String[] args) {
         View view = new View();
